5f7eb6bc4660b1abdf27d3b5978bc6ada7d2ce06
Laura Martens <laura.d.martens@icloud.com>
2022-07-11 10:32:39 +0200

    Update notebooks

---
+++ notebooks/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent.resolve()
+
+data_path = "/storage/groups/ml01/workspace/laura.martens/moretti_colab/transfer_data"
+DATA_DIR = ROOT / "data"
+FIG_DIR = ROOT / "figures"
+CACHE_DIR = ROOT / "cache"
+
