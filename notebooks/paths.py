a29a850319109d159047e9f0a018494a2171392c
Laura Martens <laura.d.martens@icloud.com>
2022-12-01 14:03:05 +0100

    Delete unused files

--- notebooks/paths.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-ROOT = Path(__file__).parent.resolve()
-
-data_path = "/storage/groups/ml01/workspace/laura.martens/moretti_colab/transfer_data"
-DATA_DIR = ROOT / "data"
-FIG_DIR = ROOT / "figures"
-CACHE_DIR = ROOT / "cache"
-
